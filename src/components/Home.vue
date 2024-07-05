b9a50091ecd8f35607dd42cc4c1dcd59d4e8ace3
Wyj-xhm <1564262185@qq.com>
2024-07-05 15:58:25 +0800

    新闻模块开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,80 +1,80 @@
 <template>
-
-    <div class="home_child_box">
-         <!-- 第一块区域————————轮播图 -->
-  <div class="block text-center">
-    <el-carousel height="40rem" autoplay :interval="3000">
-      <el-carousel-item v-for="item in arr.arr" :key="item.id">
-        <h2>文本显示</h2>
-        <img :src="item.idVieo" alt="">
-      </el-carousel-item>
-    </el-carousel>
-  </div>
-  <!-- 第二块区域——————公司宣传 -->
-  <div class="main_content">
-    <h4>公司宣传广告</h4>
-  </div>
-  <!-- 第三块区域——————公司宣传 -->
-  <div class="main_merchant">
-    <h4>公司合作商家</h4>
-  </div>
+  <div class="home_child_box">
+    <!-- 第一块区域————————轮播图 -->
+    <div class="block text-center">
+      <el-carousel height="40rem" autoplay :interval="3000">
+        <el-carousel-item v-for="item in arr.arr" :key="item.id">
+          <h2>文本显示</h2>
+          <img :src="item.idVieo" alt="" />
+        </el-carousel-item>
+      </el-carousel>
     </div>
+    <!-- 第二块区域——————公司宣传 -->
+    <div class="main_content">
+      <h4>公司宣传广告</h4>
+    </div>
+    <!-- 第三块区域——————公司宣传 -->
+    <div class="main_merchant">
+      <h4>公司合作商家</h4>
+    </div>
+    <News/>
+  </div>
 </template>
 
 <script setup lang="ts">
+import  News  from "./News.vue";
 import swpOne from "../assets/swpOne.jpg";
 import swpTwo from "../assets/swpTwo.jpg";
 
-import {reactive} from "vue"
-const arr=reactive({
-    arr:[
+import { reactive } from "vue";
+const arr = reactive({
+  arr: [
     {
-        id:1,
-        idVieo:swpOne
+      id: 1,
+      idVieo: swpOne,
     },
     {
-        id:2,
-        idVieo:swpTwo
-    }
-]
-})
+      id: 2,
+      idVieo: swpTwo,
+    },
+  ],
+});
 </script>
 
 <style scoped lang="scss">
 .demonstration {
- color: var(--el-text-color-secondary);
+  color: var(--el-text-color-secondary);
 }
 
 .el-carousel__item h3 {
- color: #475669;
- opacity: 0.75;
- line-height: 150px;
- margin: 0;
- text-align: center;
+  color: #475669;
+  opacity: 0.75;
+  line-height: 150px;
+  margin: 0;
+  text-align: center;
 }
 
 .el-carousel__item:nth-child(2n) {
- background-color: #99a9bf;
+  background-color: #99a9bf;
 }
 
 .el-carousel__item:nth-child(2n + 1) {
- background-color: #d3dce6;
+  background-color: #d3dce6;
 }
 .Slideshow_img {
- width: 100%;
- height: 100%;
+  width: 100%;
+  height: 100%;
 }
 // 第二个区域
 .main_content {
- width: 100%;
- height: 600px;
- background: url("../assets/main.jpg") 100%;
+  width: 100%;
+  height: 600px;
+  background: url("../assets/main.jpg") 100%;
 }
 // 第三个区域
 .main_merchant {
- width: 100%;
- height: 600px;
- background: url("../assets/Slideshow.jpeg") 100%;
+  width: 100%;
+  height: 600px;
+  background: url("../assets/Slideshow.jpeg") 100%;
 }
-
 </style>
---
+++ src/components/News.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="news_box">
+    <h1>公司新闻</h1>
+    <div class="card_list">
+      <div v-for="(item, index) in state.arr" :key="item.id">
+        <el-card style="max-width: 480px; height: 700px">
+          <img
+            :src="item.idVieo"
+            alt=""
+            @mouseenter="mouseenterEvent(index)"
+            @mouseleave="mouseleaveEvent(index)"
+            ref="imgRef"
+          />
+          <p class="time">2024-06-25</p>
+          <h4 class="headline">今创集团积极践行社会责任照亮乡村振兴之路</h4>
+          <span class="introduce"
+            >2024年6月20-21日，锐博集团参加了广州市天河区人社局组织的梅州兴宁市坭陂镇结对帮扶工作座谈会及捐</span
+          >
+          <el-button type="primary" class="operation">查看更多</el-button>
+        </el-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import swpOne from "../assets/swpOne.jpg";
+import swpTwo from "../assets/swpTwo.jpg";
+import { ref, reactive, onMounted } from "vue";
+import { CollectionTag } from "@element-plus/icons-vue/dist/types";
+//api
+import { getDataApi } from "@/api/request";
+const imgRef = ref();
+const state = reactive({
+    getNewsData:[],
+  arr: [
+    {
+      id: 1,
+      idVieo: swpOne,
+    },
+    {
+      id: 2,
+      idVieo: swpTwo,
+    },
+  ],
+});
+// 鼠标滑入事件
+const mouseenterEvent = (index) => {
+  imgRef.value[index].style.transform = "scale(1.1)";
+  imgRef.value[index].style.transition = "0.2s";
+  imgRef.value[index].style.borderRadius = "1rem";
+};
+// 鼠标离开事件
+const mouseleaveEvent = (index) => {
+  imgRef.value[index].style.transform = "scale(1)";
+  imgRef.value[index].style.transition = "0.2s";
+  imgRef.value[index].style.borderRadius = "0";
+};
+// 获取新闻数据
+onMounted(async () => {
+  state.getNewsData = await getDataApi();
+  console.log("1232", state.getNewsData.data.data);
+});
+
+</script>
+
+<style scoped lang="scss">
+.news_box {
+  width: 60%;
+  height: 40rem;
+  margin: 0 auto;
+  background-color: beige;
+  h1 {
+    width: 100%;
+    height: 5rem;
+    text-align: center;
+    line-height: 5rem;
+  }
+  .card_list {
+    display: flex;
+    justify-content: space-around;
+    img {
+      width: 20rem;
+      height: 15rem;
+      display: block;
+      margin: 0 auto;
+    }
+    // img:hover {
+    //   transform: scale(1.1); //设置图片按照比例放大1.3倍
+    //   transition: 0.2s;
+    //   border-radius: 1rem;
+    // }
+    .operation {
+      display: inline-block;
+    }
+  }
+}
+</style>
