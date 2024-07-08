789e4f4af4a148fc9fce20a2c457241ed8d55d1a
Wyj-xhm <1564262185@qq.com>
2024-07-08 17:33:26 +0800

    合作商家、资源服务

---
+++ src/components/CompanyAchievement.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="content">
+    <div class="headline">
+        <span >今创人力</span>
+        <div class="radius">
+            <p></p>
+        </div>
+        <span>专注全国省内人力资源服务</span>
+    </div>
+   
+    <div class="manpower">
+        <div>
+            <el-statistic  :value="outputValue1"  :value-style="{ color: 'red'}" class="my-custom-statistic">
+                <template #suffix>
+                    <p class="plus-sign">+</p>
+                </template>
+            </el-statistic>
+            <p class="introduce">省内外企事业单位</p>
+        </div>
+        <div>
+             <el-statistic  :value="outputValue2"  :value-style="{ color: 'red' }" class="my-custom-statistic">
+                <template #suffix>
+                    <p class="plus-sign">+</p>
+                </template>
+            </el-statistic>
+            <p class="introduce">各类中小微企业</p>
+        </div>
+        <div>
+             <el-statistic  :value="outputValue3"  :value-style="{ color: 'red' }" class="my-custom-statistic">
+                 <template #suffix>
+                     <div style="position: relative;">
+                        <p class="plus-sign" style="font-size: 4rem; position: absolute; bottom: -5rem; color: red;">W</p>
+                        <p class="plus-sign" style="position: absolute; right: -6rem; bottom: -2.5rem;">+</p>
+                    </div>
+                </template>
+            </el-statistic>
+            <p class="introduce" style="line-height: 5rem;">行业涉岗对口人群</p>
+        </div>
+    </div> 
+  </div>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import { useTransition } from '@vueuse/core'
+const source1 = ref(0)
+const source2 = ref(0)
+const source3 = ref(0)
+const outputValue1 = useTransition(source1, {
+  duration: 1500,
+})
+const outputValue2 = useTransition(source2, {
+  duration: 1500,
+})
+const outputValue3 = useTransition(source3, {
+  duration: 1500,
+})
+source1.value = 4600
+source2.value = 3600
+source3.value = 50
+</script>
+
+<style lang="scss" scoped>
+    .content{
+        width: 100%;
+        height: 60%;
+        .headline{
+            width: 100%;
+            height: 75%; 
+            font-size: 4rem;
+            font-weight: 700;
+            color: red;
+            display: flex;
+            justify-content: center;
+            .radius{
+                width: 4rem;
+                height: 4rem;
+                line-height: 4rem; 
+                p{  
+                    position: relative;
+                    left: 1.5rem;
+                    top: -1.5rem;
+                    width: 1rem;
+                    height: 1rem;
+                    color: red;
+                    background-color: red;
+                    border-radius: 50%; 
+                }
+            }
+          
+        }
+        .manpower{
+            display: flex;
+            justify-content: space-between;
+           .my-custom-statistic::v-deep(.el-statistic__content) {  
+                font-weight: 700;
+                font-size: 4rem; /* 设置 Statistic 组件内部数字的字体大小 */  
+            }
+            .plus-sign{
+                font-weight: 700;
+                font-size: 2rem;
+            } 
+            .introduce{
+                letter-spacing: 5px;
+                font-weight: 700;
+                font-size: 1.2rem;
+            }
+        }
+
+    }
+</style>
---
+++ src/components/LogoList.vue
@@ -0,0 +1,170 @@
+<template>
+    <div class="merchant">
+        合作商家
+    </div>
+  <div class="logo_list">
+    <div v-for="item in state.arr" :key="item.id" class="logo-list-layout">
+        <div class="main">
+            <img :src="item.idVieo" alt="" />
+        </div>
+      
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import swpOne from "../assets/th.jpg";
+import { reactive } from "vue";
+const state = reactive({
+  arr: [
+    {
+      id: 1,
+      idVieo: swpOne,
+    },
+    {
+      id: 2,
+      idVieo: swpOne,
+    },
+    {
+      id: 3,
+      idVieo: swpOne,
+    },
+    {
+      id: 4,
+      idVieo: swpOne,
+    },
+    {
+      id: 5,
+      idVieo: swpOne,
+    },
+    {
+      id: 6,
+      idVieo: swpOne,
+    },
+    {
+      id: 7,
+      idVieo: swpOne,
+    },
+    {
+      id: 8,
+      idVieo: swpOne,
+    },
+    {
+      id: 9,
+      idVieo: swpOne,
+    },
+    {
+      id: 10,
+      idVieo: swpOne,
+    },
+    {
+      id: 11,
+      idVieo: swpOne,
+    },
+    {
+      id: 12,
+      idVieo: swpOne,
+    },
+    {
+      id: 13,
+      idVieo: swpOne,
+    },
+    {
+      id: 14,
+      idVieo: swpOne,
+    },
+    {
+      id: 15,
+      idVieo: swpOne,
+    },
+    {
+      id: 16,
+      idVieo: swpOne,
+    },
+    {
+      id: 17,
+      idVieo: swpOne,
+    },
+    {
+      id: 18,
+      idVieo: swpOne,
+    },
+    {
+      id: 19,
+      idVieo: swpOne,
+    },
+    {
+      id: 20,
+      idVieo: swpOne,
+    },
+    {
+      id: 21,
+      idVieo: swpOne,
+    },
+    {
+      id: 22,
+      idVieo: swpOne,
+    },
+    {
+      id: 23,
+      idVieo: swpOne,
+    },
+    {
+      id: 24,
+      idVieo: swpOne,
+    },
+    {
+      id: 25,
+      idVieo: swpOne,
+    },
+    {
+      id: 26,
+      idVieo: swpOne,
+    },
+  ],
+});
+</script>
+
+<style lang="scss" scoped>
+.merchant{
+    font-size: 4rem;
+    color: red; 
+    text-align: center;
+}
+.logo_list {
+    margin-top: 2rem;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    overflow-y: auto;
+    flex-wrap: wrap; /* 允许内容换行 */
+    justify-content: space-between;
+    margin: 0 auto;
+    -ms-overflow-style: none;  /* IE 和 Edge */  
+    scrollbar-width: none; /* Firefox */  
+    /* Chrome, Safari, Edge */  
+    &::-webkit-scrollbar {  
+        display: none;  
+    }
+  .logo-list-layout { 
+    width: 20%;// 给子元素定宽
+    height: 50%; // height: calc(100% - 40px); 用calc 用的会比较多。
+    line-height: 10rem;
+    .main{
+        width: 4rem;
+        height: 4rem;
+        margin: 0 auto;
+      
+         img {
+            width: 100%;
+            height: 100%;
+        }
+    }
+   
+  }
+}
+
+  .logo_list ::-webkit-scrollbar {
+       display: none;
+    }
+</style>
