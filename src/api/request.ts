195c1721ce13309f783c958a5e64ff0507b49de9
Wyj-xhm <1564262185@qq.com>
2024-07-10 09:08:05 +0800

    接口请求

--- src/api/request.ts
+++ src/api/request.ts
@@ -2,4 +2,4 @@ import instance from "@/utils/request";
 
 export const loginApi=(paramss:LoginType)=>instance.post("http://192.168.101.17:1337/api/auth/local",paramss)
 
-export const getDataApi=()=>instance.get("http://192.168.101.17:1337/api/news?populate=*")
+export const getDataApi=()=>instance.get("/api/news/1")
