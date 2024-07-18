dcecca715eea42187ef13dfa4f7a9d11f06fd947
godzhu0919 <1034394984@q.com>
2024-07-18 10:48:28 +0800

    服务价值内容更改

--- src/threePage/productsPage/Flexible/index.vue
+++ src/threePage/productsPage/Flexible/index.vue
@@ -21,23 +21,29 @@ const titleList = ref();
 const state = reactive({ 
   items: [
     {
-      title: "降低企业成本：",
+      title: "提升灵活性与绩效",
       id: 0,
       isExpanded: false,
       content:
-        "精简基础性岗位，把HR从繁琐的事务性工作中解放出来，使企业人力资源工作人员能够将更多时间投入到人力资源核心业务，为企业提供更多支持，以降低企业成本，实现效率最大化。",
+        "有效提高组织战略灵活性与高绩效性",
     },
     {
-      title: "规避用工风险：",
+      title: "降低成本与资源整合",
       id: 1,
       isExpanded: false,
-      content: "全流程服务符合法律法规，提供专业指导帮助企业优化规避用工风险。",
+      content: "降低企业综合成本、整合企业优势资源",
     },
     {
-      title: "说明：",
+      title: "专注业务发展",
       id: 2,
       isExpanded: false,
-      content: "这一个部分做成模块卡片，点击卡片后会显示详细信息",
+      content: "释放职能部门，聚焦业务发展，提升企业效能",
+    },
+    {
+      title: "风险防控保障",
+      id: 3,
+      isExpanded: false,
+      content: "专业的风险防控团队，为企业权益保驾护航",
     },
   ],
 });
